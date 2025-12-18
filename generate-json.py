595d789440494aa359960e23dfaaaeda1c27e641
michaelaurelio <mikeaurelio3103@gmail.com>
2025-12-18 09:38:43 +0700

    Update the generate-json.py

--- generate-json.py
+++ generate-json.py
@@ -10,10 +10,6 @@ OG_DIR = "malwares"
 GA_DIR = "ga_dir"
 MA_DIR = "ma_dir"
 
-def save_and_exit(sig, frame):
-    with open("temp_results.json", "w", encoding="utf-8") as fout:
-        json.dump(results, fout, indent=2)
-
 for f in os.listdir(OG_DIR):
     src_path = os.path.join(OG_DIR, f)
     if os.path.isfile(src_path):
@@ -29,10 +25,8 @@ os.system(f"cd src; python3 memetic-cave.py -p ../{MA_DIR}")
 files = [f for f in os.listdir(OG_DIR) if os.path.isfile(os.path.join(OG_DIR, f))]
 model = malconv("src/malconv.h5")
 
-signal.signal(signal.signal.SIGINT, save_and_exit)
-
 results = []
-for f in files:
+for f in files[:1]:
     pred_og = model.predict(f"{OG_DIR}/{f}")
     pred_ga = model.predict(f"{GA_DIR}/{f}")
     pred_ma = model.predict(f"{MA_DIR}/{f}")
